92b306ea25c7c7befe59231cdcf5e1608abe1a62
Connor Julian <sirwulfgaar@gmail.com>
2025-06-09 21:22:10 -0400

    Pivot point. Task 1: -Isolated comments based on if the strings contained values that were highlighted in the task description. -Operates very simply but also not very functionally efficient. Needs refined so we can address part two of task one, isolating the everything else

--- index.php
+++ index.php
@@ -8,17 +8,35 @@
 </head>
 <body>
     <div class="container">
-        <h1>DB Connection Test</h1>
+        <h1>Sweetwater PHP Demo App</h1>
         <ul>
             <?php
-            // Retrieve all comments just to make sure we can 
+            // Establish connection.
             $conn = new mysqli("localhost", "root", "", "sweetwaterdemo");
-            // $result = $conn->query("SELECT * FROM sweetwater_test");
-            $candyComments = $conn->query(query:"SELECT * from sweetwater_test where comments like '%candy%'");
 
+            //Original result for grabbing all rows, might need later.
+            // $result = $conn->query("SELECT * FROM sweetwater_test");
+
+            //Super gross super basic query per area of concern IE candy, callbacks, referrals etc.
+            $candyComments = $conn->query(query:"SELECT * from sweetwater_test where comments like '%candy%'");
+            $callBackComments = $conn->query(query:"SELECT * from sweetwater_test where comments like '%call me%' or comments like '%dont call me%' or comments like '%do not call me%'");
+            $referralComments = $conn->query(query:"SELECT * from sweetwater_test where comments like '%referral%' or comments like '%referred%'");
+            $signatureComments = $conn->query(query:"SELECT * from sweetwater_test where comments like '%signature%'");
+            //Equally simple and choppy while loops to itterate over our results and slap em ina  big gross list for now.
             while ($row = $candyComments->fetch_assoc()){
                 echo "<li>" . $row['comments'];
             }
+            while($row = $callBackComments->fetch_assoc()){
+                echo "<li>". $row['comments'];
+            }
+            while($row = $referralComments->fetch_assoc()){
+                echo "<li>". $row['comments'];
+            }
+            while($row = $signatureComments->fetch_assoc()){
+                echo "<li>". $row['comments'];
+            }
+
+            //Old result to print all id's to test
             // while ($row = $result->fetch_assoc()) {
             //     echo "<li>" . $row['orderid'];
             // }
