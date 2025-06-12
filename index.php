05271dda121432978862cce44c250515eda80435
Connor Julian <sirwulfgaar@gmail.com>
2025-06-11 22:59:03 -0400

    Feedback task 1: Alongside task 2, which was done first, abstracts out into more thoghtful layers using functions to handle the big work, but keep the main flow of the app in a clearly laid out section of steps. Would be even more concise without the every single comment but for this exercise I want to be very explicit.

--- index.php
+++ index.php
@@ -22,38 +22,18 @@
             //Grab all our rows off our table for parsing.
             $result = $conn->query("SELECT * FROM sweetwater_test");
 
-            //Some empty arrays for each type of comment we want
-            $candyResults = [];
-            $callBackResults = [];
-            $referralResults = [];
-            $signatureResults = [];
-            $remainingResults = [];
-            $keyPhrase = 'Expected Ship Date: ';
+            //Names for the sections we want to build.
+            $sectionNames = array("Candy", "Calls", "Referrals", "Signatures", "Everything Else");
 
-            //While Loop to do some checks for values within the comments column of each row.
-            while ($row = $result->fetch_assoc()) {
-                if (str_contains($row['comments'], 'candy')) {
-                    array_push($candyResults, $row['comments']);
-                } elseif (str_contains($row['comments'], 'call me')) {
-                    array_push($callBackResults, $row['comments']);
-                } elseif (str_contains($row['comments'], 'dont call me')) {
-                    array_push($callBackResults, $row['comments']);
-                } elseif (str_contains($row['comments'], 'referred')) {
-                    array_push($referralResults, $row['comments']);
-                } elseif (str_contains($row['comments'], 'signature')) {
-                    array_push($signatureResults, $row['comments']);
-                } else {
-                    array_push($remainingResults, $row['comments']);
-                }
-            }
+            //Run our row result through our processing function.
+            $processedComments = processCommentsIntoBins($result);
 
             //Echos for each array of results.
-            echoArrayValues($candyResults, "Candy");
-            echoArrayValues($callBackResults, "Calls");
-            echoArrayValues($referralResults, "Referrals");
-            echoArrayValues($signatureResults, "Signatures");
-            echoArrayValues($remainingResults, "Everything Else");
+            foreach ($processedComments as $index => $commentBin) {
+                echoArrayValues($commentBin, $sectionNames[$index]);
+            }
 
+            //Close our connection.
             $conn->close();
 
             //Function to echo all our values from our arrays without taking up so much space.
@@ -66,6 +46,48 @@
                 echo "<br>\n<br>\n";
             }
 
+            //Function to take all our comment data and turn them into processed bins. 
+            //Returns a 2d Array of the bins for use in building our tables of comments.
+            function processCommentsIntoBins($result): array
+            {
+                //Some empty arrays for each type of comment we want.
+                //To update: add a new bin, and update the while loop.
+                $candyResults = [];
+                $callBackResults = [];
+                $referralResults = [];
+                $signatureResults = [];
+                $remainingResults = [];
+
+                //Itterative loop to take our results and push them into the correct bins.
+                while ($row = $result->fetch_assoc()) {
+                    if (str_contains($row['comments'], 'candy')) {
+                        array_push($candyResults, $row['comments']);
+                    } elseif (str_containsTwo($row['comments'], ['call me', 'dont call me'])) {
+                        array_push($callBackResults, $row['comments']);
+                    } elseif (str_contains($row['comments'], 'referred')) {
+                        array_push($referralResults, $row['comments']);
+                    } elseif (str_contains($row['comments'], 'signature')) {
+                        array_push($signatureResults, $row['comments']);
+                    } else {
+                        array_push($remainingResults, $row['comments']);
+                    }
+                }
+
+                //Return an array of our bins.
+                return [$candyResults, $callBackResults, $referralResults, $signatureResults, $remainingResults];
+            }
+
+            //Function to help us out when we need to search a "haystack" for two or more "needles".
+            function str_containsTwo(string $haystack, array $needles)
+            {
+                //For every needle, check if its somewhere in that darn haystack. As currently implemented, operates as an OR statement. 
+                foreach ($needles as $needle) {
+                    if (str_contains($haystack, $needle)) {
+                        return true;
+                    }
+                }
+                return false;
+            }
             ?>
         </ul>
     </div>
